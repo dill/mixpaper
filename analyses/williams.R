711a83a81130a95189d8c6d7f0a706c2f870e712
David Lawrence Miller <dave@ninepointeightone.net>
2011-07-07 13:26:22 +0100

    some figures...

--- analyses/williams.R
+++ analyses/williams.R
@@ -15,7 +15,7 @@ hs.fit1<-fitmix(hs,mix.terms=1,width=500)
 hs.best<-step.ds.mixture(hs.fit1)
 
 # plot that
-plot(hs.best,style="hist", breaks=c(0,11,22,33,43,65,87,109,130,152,174,196,217,239,261,283,304,326,348,370,391,413,435,457,478,500),main="Harbour seal (in water)")
+plot(hs.best, breaks=c(0,11,22,33,43,65,87,109,130,152,174,196,217,239,261,283,304,326,348,370,391,413,435,457,478,500),main="Harbour seal (in water)",style="comp")
 
 
 ###########################
@@ -27,7 +27,7 @@ hp.fit1<-fitmix(hp,mix.terms=1,width=500)
 hp.best<-step.ds.mixture(hp.fit1)
 
 # plot that
-plot(hp.best,style="hist", breaks=c(0,20,40,61,82,124,166,208,250,291,333,375,416,458,500),main="Harbour porpoise")
+plot(hp.best, breaks=c(0,20,40,61,82,124,166,208,250,291,333,375,416,458,500),main="Harbour porpoise",style="comp")
 
 
 ###########################
@@ -39,5 +39,5 @@ hb.fit1<-fitmix(hb,mix.terms=1,width=2000)
 hb.best<-step.ds.mixture(hb.fit1)
 
 # plot that
-plot(hb.best,style="hist", breaks=c(0, 83.5, 167, 250.5, 333, 500, 667, 833, 1000, 1167, 1333, 1500, 1667, 1833, 2000),main="Humpback")
+plot(hb.best, breaks=c(0, 83.5, 167, 250.5, 333, 500, 667, 833, 1000, 1167, 1333, 1500, 1667, 1833, 2000),main="Humpback",style="comp")
 
