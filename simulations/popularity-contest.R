56586cf852928cf786f78e6f212e2128ab6a7c0d
David Lawrence Miller <dave@ninepointeightone.net>
2012-04-26 15:25:51 -0400

    pre- flipper pull

--- simulations/popularity-contest.R
+++ simulations/popularity-contest.R
@@ -17,27 +17,25 @@ c("n.samp","sim","aic","pa","Nhat","N","mixpart","model")
 )
 
 
-todo<-c(1,2,3,4)
+todo<-4#1:3
 #todo<-1:4 
+
 
 for(combi in todo){
 
   big.pop<-c()
 
-  simn<-sim.combs$number[combi]
+  simn<-1#sim.combs$number[combi]
   type<-sim.combs$types[combi]
   start<-sim.combs$start[combi]
   end<-sim.combs$end[combi]
   
   for(pari in 1:simn){
     # nocov
-    dat<-read.csv(paste(type,"//",start,pari,end,sep=""))
+    dat<-read.csv(paste(type,"/",start,pari,end,sep=""))
     dat<-dat[,-1]
 
     names(dat)<-headers[[combi]]
-    
-    dat$Nhat<-as.double(as.character(dat$Nhat))
-    
     
     # some things got messed up, not sure why...
     if(combi==4){
@@ -49,14 +47,29 @@ for(combi in todo){
     dat.Nhat<-c()
     dat.n.samp<-c()
 
+    # put some data together
     for(model.names in unique(dat$model)){
-      dat.aic<-cbind(dat.aic,as.numeric(as.character(dat$aic[dat$model==model.names])))
-      dat.N<-cbind(dat.N,as.numeric(as.character(dat$N[dat$model==model.names])))
-      dat.Nhat<-cbind(dat.Nhat,as.numeric(as.character(dat$Nhat[dat$model==model.names])))
-      dat.n.samp<-cbind(dat.n.samp,as.numeric(as.character(dat$n.samp[dat$model==model.names])))
+      dat.aic<-cbind(dat.aic,
+                   as.numeric(as.character(dat$aic[dat$model==model.names])))
+      dat.N<-cbind(dat.N,
+                 as.numeric(as.character(dat$N[dat$model==model.names])))
+      dat.Nhat<-cbind(dat.Nhat,
+                    as.numeric(as.character(dat$Nhat[dat$model==model.names])))
+      dat.n.samp<-cbind(dat.n.samp,
+                  as.numeric(as.character(dat$n.samp[dat$model==model.names])))
     }
+    # find the AIC-best models
+    winners <- apply(dat.aic,1,which.min)
+    these.winners <- unique(dat$model)[winners]
 
-    winners<-apply(dat.aic,1,which.min)
+    # now re-code the models so that we are just comparing mmds and (m)cds
+    if(combi==1 | combi==2 ){
+      levels(these.winners)<-c("CDS","CDS","MMDS","MMDS")
+    }else if(combi==3){
+      levels(these.winners)<-c("CDS","CDS","MMDS")
+    }else if(combi==4){
+      levels(these.winners)<-c(rep("CDS",4),"MMDS",rep("CDS",6),"MMDS")
+    }
 
     # cheatcode to pull the winners out of the other data
     # bind the winners onto the end, then use the element 
@@ -65,7 +78,7 @@ for(combi in todo){
     this.Nhat<-apply(cbind(dat.Nhat,winners),1,function(x){x[x[length(x)]]})
     this.n.samp<-apply(cbind(dat.n.samp,winners),1,function(x){x[x[length(x)]]})
   
-    pop<-data.frame(winner=unique(dat$model)[winners],
+    pop<-data.frame(winner=these.winners,
                     pari=rep(pari,length(winners)),
                     type=rep(type,length(winners)),
                     n.samp=this.n.samp,
@@ -80,7 +93,8 @@ for(combi in todo){
 
   # actually do the plotting
   p <- ggplot(big.pop)
-  p <- p + geom_boxplot(aes(y=p.prb,x=as.factor(winner)))
+#  p <- p + geom_boxplot(aes(y=p.prb,x=as.factor(winner)))
+  p <- p + geom_boxplot(aes(y=Nhat,x=as.factor(winner)))
   p <- p + facet_grid(pari~n.samp)
   p<-p+opts(panel.grid.major=theme_blank(),
             panel.grid.minor=theme_blank(),
