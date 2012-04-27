84edea1d76d289c9a2ec91ebff55b8c79e23c0ee
David Lawrence Miller <dave@ninepointeightone.net>
2012-04-26 21:12:16 -0400

    AAAAAAAAAAAAAAARRRRRRRRRRGGGGGGGGGGGGHHHHHHHHHHHHH
    TOOTH PAIN!

--- simulations/popularity-contest.R
+++ simulations/popularity-contest.R
@@ -1,13 +1,12 @@
 # what won?
 library(ggplot2)
 
-
-
 sim.combs<-data.frame(types=c("nocov","pt","3point","covar"),
                       number=c(4,4,2,2),
                       start=c("BFGS+SANN-","BFGS+SANN-","BFGS+SANN-","covsim"),
                       end=c("-960-results.csv","-pt-results.csv",
-                            "-960-3pt-results.csv","-BFGS+SANN.csv"))
+                            "-960-3pt-results.csv","-BFGS+SANN.csv"),
+                      n.y=c(-2.1,-3.6,-1.75,-0.5))
 
 headers<-list(
 c("model","par.ind","n.samp","sim","par1","par2","par3","pa","aic","Nhat","N"),
@@ -16,19 +15,18 @@ c("model","par.ind","n.samp","sim","ll","aic","pa","Nhat","N","mixterms"),
 c("n.samp","sim","aic","pa","Nhat","N","mixpart","model")
 )
 
-
-todo<-4#1:3
-#todo<-1:4 
-
+todo<-1:4 
 
 for(combi in todo){
 
   big.pop<-c()
+  win.labels<-c()
 
-  simn<-1#sim.combs$number[combi]
+  simn<-sim.combs$number[combi]
   type<-sim.combs$types[combi]
   start<-sim.combs$start[combi]
   end<-sim.combs$end[combi]
+  n.y<-sim.combs$n.y[combi]
   
   for(pari in 1:simn){
     # nocov
@@ -68,7 +66,8 @@ for(combi in todo){
     }else if(combi==3){
       levels(these.winners)<-c("CDS","CDS","MMDS")
     }else if(combi==4){
-      levels(these.winners)<-c(rep("CDS",4),"MMDS",rep("CDS",6),"MMDS")
+      levels(these.winners)<-c(rep("MCDS",4),"CMMDS","CDS","MCDS","MCDS",
+                                "CDS","MCDS","MCDS","MMDS")
     }
 
     # cheatcode to pull the winners out of the other data
@@ -89,12 +88,49 @@ for(combi in todo){
                    )
   
     big.pop <- rbind(big.pop,pop)
+
+    this.win <- c()
+
+    if(combi==1 | combi==2 | combi==3){
+      ermcds<-"CDS"
+      for(win.n.samp in unique(pop$n.samp)){
+
+        this.win<-rbind(this.win,
+                        data.frame(
+                                winner = c(ermcds,"MMDS"),
+                                pari = rep(pari,2),
+                                type = rep(type,2), 
+                                n.samp=rep(win.n.samp,2),
+                                n.winner=c(sum(as.character(pop$winner[pop$n.samp==win.n.samp])==ermcds),
+                                           sum(as.character(pop$winner[pop$n.samp==win.n.samp])=="MMDS"))
+                                  )
+                       )
+      }
+    }else{
+      for(win.n.samp in unique(pop$n.samp)){
+
+        this.win<-rbind(this.win,
+                        data.frame(
+                                winner = c("CDS","CMMDS","MCDS","MMDS"),
+                                pari = rep(pari,4),
+                                type = rep(type,4),
+                                n.samp=rep(win.n.samp,4),
+                                n.winner=c(
+              sum(as.character(pop$winner[pop$n.samp==win.n.samp])=="CDS"),
+              sum(as.character(pop$winner[pop$n.samp==win.n.samp])=="CMMDS"),
+              sum(as.character(pop$winner[pop$n.samp==win.n.samp])=="MCDS"),
+              sum(as.character(pop$winner[pop$n.samp==win.n.samp])=="MMDS")
+                                  ))
+                       )
+      }
+    }
+
+    win.labels<-rbind(win.labels,this.win)
   }
 
   # actually do the plotting
   p <- ggplot(big.pop)
-#  p <- p + geom_boxplot(aes(y=p.prb,x=as.factor(winner)))
-  p <- p + geom_boxplot(aes(y=Nhat,x=as.factor(winner)))
+  p <- p + geom_boxplot(aes(y=p.prb,x=as.factor(winner)))
   p <- p + facet_grid(pari~n.samp)
   p<-p+opts(panel.grid.major=theme_blank(),
             panel.grid.minor=theme_blank(),
@@ -104,9 +140,12 @@ for(combi in todo){
             panel.background=theme_rect())
   p<-p+labs(x="Sample size",y="rel. bias in p")
   p<-p+geom_hline(yintercept=0,col="grey")
+  p<-p+geom_text(aes(x=factor(winner),y=n.y,label=n.winner),
+                      size=3,data=win.labels,col="grey")
+
+  quartz()
   print(p)
 #  ggsave(paste("boxplots-",type,".pdf",sep=""))
-  quartz()
 
 }
 
