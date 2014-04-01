83a11a3966ffdd1fc0c01f5f061298af741fb416
Dave Miller <dave@isbjorn.mcs.st-and.ac.uk>
2014-04-01 18:50:55 +0100

    new nocov results

--- simulations/nocov/runsim.R
+++ simulations/nocov/runsim.R
@@ -79,7 +79,7 @@ results<-foreach(par.ind = 1:dim(parmat)[1], .combine=rbind,
       # CDS - hn+cos (scale scaling)
 
       fit<-try(ds(sim.data,width,monotonicity="strict"))
-      if(all(class(fit$ddf)!="try-error")){
+      if(all(class(fit)!="try-error")){
         res<-rbind(res,c("cds-hnc",par.ind,n.samp,sim,rep(NA,3),
                          fitted(fit$ddf)[1],fit$ddf$criterion,
                          fit$ddf$Nhat,true.N))
@@ -92,7 +92,7 @@ results<-foreach(par.ind = 1:dim(parmat)[1], .combine=rbind,
 
       fit<-try(ds(sim.data,width,monotonicity="strict",key="hr",
                   adjustment="poly"))
-      if(all(class(fit$ddf)!="try-error")){
+      if(all(class(fit)!="try-error")){
         res<-rbind(res,c("cds-hrp",par.ind,n.samp,sim,rep(NA,3),
                          fitted(fit$ddf)[1],fit$ddf$criterion,
                          fit$ddf$Nhat,true.N))
@@ -104,7 +104,7 @@ results<-foreach(par.ind = 1:dim(parmat)[1], .combine=rbind,
       # CDS - hn+cos (width scaling)
 
       fit<-try(ds(sim.data,width,monotonicity="strict",scale="width"))
-      if(all(class(fit$ddf)!="try-error")){
+      if(all(class(fit)!="try-error")){
         res<-rbind(res,c("cds-hnc-w",par.ind,n.samp,sim,rep(NA,3),
                          fitted(fit$ddf)[1],fit$ddf$criterion,
                          fit$ddf$Nhat,true.N))
@@ -117,7 +117,7 @@ results<-foreach(par.ind = 1:dim(parmat)[1], .combine=rbind,
 
       fit<-try(ds(sim.data,width,monotonicity="strict",key="hr",
                   adjustment="poly",scale="width"))
-      if(all(class(fit$ddf)!="try-error")){
+      if(all(class(fit)!="try-error")){
         res<-rbind(res,c("cds-hrp-w",par.ind,n.samp,sim,rep(NA,3),
                          fitted(fit$ddf)[1],fit$ddf$criterion,
                          fit$ddf$Nhat,true.N))
@@ -130,7 +130,7 @@ results<-foreach(par.ind = 1:dim(parmat)[1], .combine=rbind,
 
       #fit<-try(ds(sim.data,width,monotonicity="strict",key="unif",
       #            adjustment="cos"))
-      #if(all(class(fit$ddf)!="try-error")){
+      #if(all(class(fit)!="try-error")){
       #  res<-rbind(res,c("cds-uc",par.ind,n.samp,sim,rep(NA,3),
       #                   fitted(fit$ddf)[1],fit$ddf$criterion,
       #                   true.N,fit$ddf$Nhat))
